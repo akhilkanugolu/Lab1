f4397a0ef8d83ccfd79be23e82935c956921c0ca
akhilkanugolu <60271171+akhilkanugolu@users.noreply.github.com>
2020-03-09 23:41:39 -0500

    Update lab1_q2.py

--- lab1_q2.py
+++ lab1_q2.py
@@ -1,3 +1,4 @@
+#Concatination Using concate function
 def concate(dict1, dict2): 
     result_dict = {**dict1, **dict2} 
     return result_dict 
@@ -8,5 +9,6 @@ dict2 = {'q': 2, 's': 9}
 dict3 = concate(dict1, dict2) 
 print(dict3) 
 
+#Sorting Based on Value
 for key, value in sorted(dict3.items(), key=lambda item: item[1]):
     print("%s: %s" % (key, value))
